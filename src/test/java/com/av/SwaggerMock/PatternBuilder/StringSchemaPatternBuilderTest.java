2e129e262069953b80342bcffb8506c333b69db4
avekassy1 <vekassy.andras@gmail.com>
2025-06-01 18:45:54 +0100

    #2 Pushing the StringSchemaPatternBuilderTest class

---
+++ src/test/java/com/av/SwaggerMock/PatternBuilder/StringSchemaPatternBuilderTest.java
@@ -0,0 +1,101 @@
+package com.av.SwaggerMock.PatternBuilder;
+
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+import com.github.tomakehurst.wiremock.matching.RegexPattern;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringSchemaPatternBuilderTest {
+
+    @Autowired
+    private StringSchemaPatternBuilder builder;
+
+    @BeforeEach
+    void setUp() {
+        builder = new StringSchemaPatternBuilder();
+    }
+
+    @Test
+    void testSupportsStringSchema() {
+        assertTrue(builder.supports(new StringSchema()));
+    }
+
+    @Test
+    void testExampleIsUsed() {
+        StringSchema schema = new StringSchema();
+        schema.setExample("Example string");
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof EqualToPattern);
+        assertEquals("Example string", ((EqualToPattern) pattern).getExpected());
+    }
+
+    @Test
+    void testDefaultIsUsedWhenNoExample() {
+        StringSchema schema = new StringSchema();
+        schema.setDefault("Default string");
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof EqualToPattern);
+        assertEquals("Default string", ((EqualToPattern) pattern).getExpected());
+    }
+
+    @Test
+    void testSingleEnumIsUsedAsConstant() {
+        StringSchema schema = new StringSchema();
+        schema.setEnum(Collections.singletonList("ONLY_ONE"));
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof EqualToPattern);
+        assertEquals("ONLY_ONE", ((EqualToPattern) pattern).getExpected());
+    }
+
+    @Test
+    void testMultipleEnumsCreatesRegexPattern() {
+        StringSchema schema = new StringSchema();
+        schema.setEnum(Arrays.asList("ENUM1", "ENUM2", "ENUM3"));
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof RegexPattern);
+        assertEquals("ENUM1|ENUM2|ENUM3", ((RegexPattern) pattern).getExpected());
+    }
+
+    @Test
+    void testRegexPatternUsed() {
+        StringSchema schema = new StringSchema();
+        schema.setPattern("^\\d{3}-\\d{2}-\\d{4}$");
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof RegexPattern);
+        assertEquals("^\\d{3}-\\d{2}-\\d{4}$", ((RegexPattern) pattern).getExpected());
+    }
+
+    @Test
+    void testMinAndMaxLengthPattern() {
+        StringSchema schema = new StringSchema();
+        schema.setMinLength(3);
+        schema.setMaxLength(10);
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof RegexPattern);
+        assertEquals("^.{3,10}$", ((RegexPattern) pattern).getExpected());
+    }
+
+    @Test
+    void testFallbackToMatchAnything() {
+        StringSchema schema = new StringSchema();
+
+        StringValuePattern pattern = builder.create(schema);
+        assertTrue(pattern instanceof RegexPattern);
+        assertEquals(".*", ((RegexPattern) pattern).getExpected());
+    }
+}
