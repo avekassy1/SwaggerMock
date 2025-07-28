6ef2e9ccd955b19bff4bdbad4c86a9539ff29c71
avekassy1 <vekassy.andras@gmail.com>
2025-07-28 11:00:45 +0100

    #13 Resolve formatting violations

--- src/main/java/com/av/SwaggerMock/utils/Constants.java
+++ src/main/java/com/av/SwaggerMock/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.av.SwaggerMock.utils;
 
 public class Constants {
-    public static final int EXAMPLE_INT = 123;
-    public static final String EXAMPLE_STRING = "Example text";
+  public static final int EXAMPLE_INT = 123;
+  public static final String EXAMPLE_STRING = "Example text";
 }
